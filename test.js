190589779282e0b38f8371f7824927b1cbdf0ebb
Bonapart <mibuchi773@mail.ru>
2021-12-30 01:08:25 +0600

    hz

--- test.js
+++ test.js
@@ -1,5 +1,17 @@
-let lol = [1, 2, 3, 4];
+A =  [ 20, 1, -1, 2, -2, 3, 3, 5, 5, 1, 2, 4, 20, 4, -1, -2, 5 ]
 
-for(let i = 0; i < lol.length; ++i) {
-    console.log(lol[i]);
+let cnt = {}
+
+for (i of A) {
+    if(i in cnt) {
+        cnt[i]++;
+    } else {
+        cnt[i] = 1;
+    }
+}
+
+for (i in cnt) {
+    if(cnt[i] % 2 != 0) {
+        console.log(int(i));
+    }
 }
